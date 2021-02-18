9527f6d19f591b67b281334bf9dbc8e309d9e1d8
Ilker Erbil <60328928+ilkererbil@users.noreply.github.com>
2021-02-18 13:58:11 -0800

    Update cachematrix.R
    Remove the proc.time functions and added short descriptions as per the instructions

--- cachematrix.R
+++ cachematrix.R
@@ -2,7 +2,14 @@
 ## functions do
 
 ## Write a short comment describing this function
-# 
+#####
+# This function intializes two objects, x as a matrix and m as null
+# these are then set with an object from the parent environment as y and null, respectively
+#   this allows assignment of x from parent environment and clearing of m
+# this allows matrix x to be retrieved from the parent environment via get
+# setMatrix then retrieves solution solve from the parent environment as a cache
+# getMatrix then sets m as the matrix from the parent environment as a cache
+# these four functions are all returned within a list to the parent environment
 
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
@@ -20,12 +27,16 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
-# takes an input of a matrix object
-# 
+#####
+# use the function getMatrix to set x as m
+#   m originally set as NULL in makeCacheMatrix
+# check to see if x stored in cache with if(!is.null(m))
+# if stored in cache, return solved matrix
+# otherwise, continue the by getting the matrix, solving, adding to the cache via
+#   the $set function, and returning the inverted matrix
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
-  ptm <- proc.time()
   m <- x$getMatrix()
   if(!is.null(m)) {
     message("getting cached data")
@@ -34,6 +45,5 @@ cacheSolve <- function(x, ...) {
   data <- x$get()
   m <- solve(data, ...)
   x$setMatrix(m)
-  print(proc.time()-ptm)
   m
 }
